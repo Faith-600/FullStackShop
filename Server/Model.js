bcabe1df532851aff1c4c30f38e2a4b63560c739
Ezekiel Faith <ezekielfaith2001@gmail.com>
2025-01-25 20:47:04 +0100

    Update Model.js

--- Server/Model.js
+++ Server/Model.js
@@ -1,12 +1,1 @@
-const mongoose = require('mongoose')
 
-const loginSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
-
-})
-
-const Login = mongoose.model("login",loginSchema)
-
-module.exports = Login
