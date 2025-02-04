629dcaa28923dc9d2ffccf1b89e085fa79692768
Ezekiel Faith <ezekielfaith2001@gmail.com>
2025-02-04 21:01:37 +0100

    made a correction

--- src/assets/Components/Welcome.jsx
+++ src/assets/Components/Welcome.jsx
@@ -29,7 +29,7 @@ const fetchItems = async () =>{
     const response = await axios.get('https://full-stack-shop-backend.vercel.app/posts');
     console.log(response.data.posts);
     // console.log(username)
-  setPosts(response.data.posts)
+  setPosts(response.data.posts || [])
   }
   catch(error){
      console.error('Error fetching posts:', error);
