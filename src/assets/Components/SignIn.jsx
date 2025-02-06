4b7456ab322dd2fe39cc7541ef113a6bd0a1e300
Ezekiel Faith <ezekielfaith2001@gmail.com>
2025-02-06 20:29:31 +0100

    made a correction

--- src/assets/Components/SignIn.jsx
+++ src/assets/Components/SignIn.jsx
@@ -132,11 +132,11 @@ function SignIn() {
             >
               Login
             </button>
-            <button
+            <Link to ="/welcome"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-2"
             >
               Guest User
-            </button>
+            </Link>
           </div>
         </form>
 
