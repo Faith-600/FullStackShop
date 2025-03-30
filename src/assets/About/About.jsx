661c83046a3f96fe307a13d80ed09ad28060c0ff
Ezekiel Faith <ezekielfaith2001@gmail.com>
2025-03-30 18:50:40 +0100

    Added an about page

--- src/assets/About/About.jsx
+++ src/assets/About/About.jsx
@@ -16,6 +16,7 @@ const About = () => {
       <h1 className="text-2xl font-bold mt-6">Our Story</h1>
       <p className="text-lg leading-7 p-4 text-center max-w-3xl">
         Hi 👋🏽👋🏻👋🏿, I'm Faith Ezekiel, the creator of CircleKonnect.
+        
         CircleKonnect is my first major project since transitioning into tech. What started as a simple login and signup project soon grew into something bigger.
         I've always been curious about my friends' wider circles, wanting to connect with their friends and get to know them better. That curiosity inspired me to build CircleConnect, 
         a platform designed to bridge connections, expand friendships, and help people discover more about their extended networks.
